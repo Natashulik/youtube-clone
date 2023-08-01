a57df1a7486e19d2062407eca04bb8ae5a7c8eea
Natasha Viktorovich <nata17v@mail.ru>
2023-08-01 10:30:38 +0300

    refactor: change home-page adress

--- src/hoc/PrivateRoute.js
+++ src/hoc/PrivateRoute.js
@@ -2,7 +2,7 @@ import { Navigate } from "react-router-dom";
 
 const PrivateRoute = ({children}) => {
 
-  return localStorage.getItem("token") ? children : <Navigate to={"/login"}/>
+  return localStorage.getItem("token") ? children : <Navigate to={"/youtube-clone"}/>
 
 }
 
