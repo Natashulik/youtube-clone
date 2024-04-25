8b4601bb62ff5a9299f38fe7b557d3c04e23466a
Natashulik <nata17v@mail.ru>
2024-04-25 20:29:23 +0300

    fix: correct mobile styles

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,23 +4,23 @@
 //email: "1111@tut.by"
 //password: "1111Aa+++Aa"
 
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
-import { Button, Form, Input } from 'antd';
+import React from "react";
+import { Link } from "react-router-dom";
+import { EyeInvisibleOutlined, EyeTwoTone } from "@ant-design/icons";
+import { Button, Form, Input } from "antd";
 import { useNavigate } from "react-router-dom";
-import logo from '../images/logo.svg'
-import { setInputText } from '../redux/inputSlice';
-import { setFavorites } from '../redux/favoriteSlice';
+import logo from "../images/logo.svg";
+import { setInputText } from "../redux/inputSlice";
+import { setFavorites } from "../redux/favoriteSlice";
 import { useSelector, useDispatch } from "react-redux";
 import { setEmail, setPassword, setIsError } from "../redux/loginSlice";
-import { loadFavorites } from '../helpers/localStorage';
-import { fetchLogin } from '../helpers/fetchLogin';
+import { loadFavorites } from "../helpers/localStorage";
+import { fetchLogin } from "../helpers/fetchLogin";
 
 function Login() {
-  const email = useSelector(state => state.login.email);
-  const password = useSelector(state => state.login.password);
-  const isError = useSelector(state => state.login.isError);
+  const email = useSelector((state) => state.login.email);
+  const password = useSelector((state) => state.login.password);
+  const isError = useSelector((state) => state.login.isError);
   const dispatch = useDispatch();
 
   const navigate = useNavigate();
@@ -31,62 +31,79 @@ function Login() {
 
   const handleSubmit = async (event) => {
     try {
-     const data = await fetchLogin(email, password);
+      const data = await fetchLogin(email, password);
 
       if (!data.token) {
         throw new Error(data.message);
       } else {
         localStorage.setItem("token", data.token);
         navigate("/search");
-        dispatch(setInputText(''));
+        dispatch(setInputText(""));
         dispatch(setFavorites(loadFavorites()));
       }
     } catch (error) {
-      console.log(error.message)
+      console.log(error.message);
       dispatch(setIsError(true));
     }
-  }
+  };
 
-  return <div className='login_wrapper'>
-    <img src={logo} alt="logo" className='logo' />
-    <h3 className='form_title'>Вход</h3>
+  return (
+    <div className="login">
+      <div className="login_wrapper">
+        <img src={logo} alt="logo" className="logo" />
+        <h3 className="form_title">Вход</h3>
 
-    <Form className='login_form' layout="vertical"
-      name="basic"
-      initialValues={{ remember: true }}
-      onFinish={onFinish}
-      autoComplete="off"
-    >
+        <Form
+          className="login_form"
+          layout="vertical"
+          name="basic"
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+          autoComplete="off"
+        >
+          <div className="login_block">
+            <p className="field_name">Эл.почта</p>
+            <Input
+              className="log_input"
+              value={email}
+              onChange={(event) => dispatch(setEmail(event.target.value))}
+            />
+          </div>
 
-      <div className="login_block">
-        <p className="field_name">Эл.почта</p>
-        <Input className="log_input" value={email} onChange={event => dispatch(setEmail(event.target.value))} />
+          <div className="password_block">
+            <p className="field_name"> Пароль</p>
+            <Input.Password
+              className="password_input"
+              iconRender={(visible) =>
+                visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
+              }
+              value={password}
+              onChange={(event) => dispatch(setPassword(event.target.value))}
+            />
+          </div>
+
+          <Form.Item>
+            <Button type="primary" htmlType="submit" className="button_login">
+              Войти
+            </Button>
+          </Form.Item>
+        </Form>
+
+        <div className="redirect_block">
+          <span>Нет аккаунта? </span>
+          <Link to="/register" className="redirect_link">
+            Создать аккаунт
+          </Link>
+        </div>
+
+        <div className="error_block">
+          {isError && (
+            <p className="error-message"> Неверные эл.почта или пароль!</p>
+          )}
+        </div>
       </div>
-
-      <div className="password_block">
-        <p className="field_name"> Пароль</p>
-        <Input.Password className="password_input"
-          iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
-          value={password}
-          onChange={event => dispatch(setPassword(event.target.value))} />
-      </div>
-
-      <Form.Item>
-        <Button type="primary" htmlType="submit" className="button_login">
-          Войти
-        </Button>
-      </Form.Item>
-    </Form>
-
-    <div className="redirect_block">
-      <span>Нет аккаунта?  </span>
-      <Link to="/register" className="redirect_link">Создать аккаунт</Link>
     </div>
-
-    <div className="error_block">
-      {isError && (<p className="error-message"> Неверные эл.почта или пароль!</p>)}
-    </div>
-  </div>
+  );
 }
 
 export default Login;
--- src/pages/Registration.js
+++ src/pages/Registration.js
@@ -1,12 +1,21 @@
 import { useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { setUsername, setEmail, setPassword, setAge, setGender, setIsError } from "../redux/registrationSlice";
-import { setInputText } from '../redux/inputSlice';
-import logo from '../images/logo.svg'
+import {
+  setUsername,
+  setEmail,
+  setPassword,
+  setAge,
+  setGender,
+  setIsError,
+} from "../redux/registrationSlice";
+import { setInputText } from "../redux/inputSlice";
+import logo from "../images/logo.svg";
 import { fetchLogin } from "../helpers/fetchLogin";
 
 function Registration() {
-  const {username,email, password, age, gender, isError} = useSelector(state => state.registration);
+  const { username, email, password, age, gender, isError } = useSelector(
+    (state) => state.registration
+  );
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -16,16 +25,16 @@ function Registration() {
       const result = await fetch(process.env.REACT_APP_URL_REGISTER, {
         method: "POST",
         headers: {
-          "Content-Type": "application/json"
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
           username: username,
           email: email,
           password: password,
           gender: gender,
-          age: age
-        })
-      })
+          age: age,
+        }),
+      });
 
       const data = await result.json();
       if (!data.id) {
@@ -34,58 +43,97 @@ function Registration() {
         const data = await fetchLogin(email, password);
         localStorage.setItem("token", data.token);
         navigate("/search");
-        dispatch(setInputText(''));
+        dispatch(setInputText(""));
       }
-
     } catch (error) {
       dispatch(setIsError(true));
     }
-  }
+  };
 
-  return (<div className='registration_wrapper'>
-    <img src={logo} alt="logo" className='logo' />
-    <h3 className='form_title'>Регистрация</h3>
-    <form onSubmit={event => handleSubmit(event)}>
-      <div className="form_register">
+  return (
+    <div className="registration">
+      <div className="registration_wrapper">
+        <img src={logo} alt="logo" className="logo" />
+        <h3 className="form_title">Регистрация</h3>
+        <form onSubmit={(event) => handleSubmit(event)}>
+          <div className="form_register">
+            <div className="username_block">
+              <p className="field_name">Логин</p>
+              <input
+                value={username}
+                onChange={(event) => dispatch(setUsername(event.target.value))}
+                className="log_input"
+              />
+            </div>
 
-        <div className="username_block">
-          <p className="field_name">Логин</p>
-          <input value={username} onChange={event => dispatch(setUsername(event.target.value))} className="log_input" />
-        </div>
+            <div className="email_block">
+              <p className="field_name">Эл.почта</p>
+              <input
+                value={email}
+                onChange={(event) => dispatch(setEmail(event.target.value))}
+                className="log_input"
+              />
+            </div>
 
-        <div className="email_block">
-          <p className="field_name">Эл.почта</p>
-          <input value={email} onChange={event => dispatch(setEmail(event.target.value))} className="log_input" />
-        </div>
+            <div className="password_block">
+              <p className="field_name">Пароль*</p>
+              <input
+                value={password}
+                type="password"
+                onChange={(event) => dispatch(setPassword(event.target.value))}
+                className="log_input"
+              />
+            </div>
 
-        <div className="password_block">
-          <p className="field_name">Пароль*</p>
-          <input value={password} type="password" onChange={event => dispatch(setPassword(event.target.value))} className="log_input" />
-        </div>
+            <div className="age_block">
+              <p className="field_name">Возраст</p>
+              <input
+                value={age}
+                onChange={(event) => dispatch(setAge(event.target.value))}
+                className="log_input"
+              />
+            </div>
 
-        <div className="age_block">
-          <p className="field_name">Возраст</p>
-          <input value={age} onChange={event => dispatch(setAge(event.target.value))} className="log_input" />
-        </div>
+            <div className="gender_block">
+              <span className="field_name field_gender">Пол</span>
+              <label className="radio_button">
+                <input
+                  value="male"
+                  checked={gender === "male"}
+                  onChange={(event) => dispatch(setGender(event.target.value))}
+                  type="radio"
+                  name="gender"
+                />{" "}
+                male{" "}
+              </label>
+              <label className="radio_button">
+                <input
+                  value="female"
+                  checked={gender === "female"}
+                  onChange={(event) => dispatch(setGender(event.target.value))}
+                  type="radio"
+                  name="gender"
+                />{" "}
+                female{" "}
+              </label>
+            </div>
 
-        <div className="gender_block">
-          <span className="field_name field_gender">Пол</span>
-          <label className="radio_button">
-            <input value="male" checked={gender === 'male'} onChange={event => dispatch(setGender(event.target.value))} type="radio" name="gender" /> male </label>
-          <label className="radio_button">
-            <input value="female" checked={gender === 'female'} onChange={event => dispatch(setGender(event.target.value))} type="radio" name="gender" /> female </label>
-        </div>
-
-        <button type="submit" className="button_register">SIGN UP</button>
-        <span className="password_info">*минимум 8 знаков, 1 заглавный, 1 строчный,</span>
-        <span className="password_info second">1 число и 1 символ</span>
-        <div className="error_block">
-          {isError && (<p className="error-message"> Неверная информация!</p>)}
-        </div>
+            <button type="submit" className="button_register">
+              SIGN UP
+            </button>
+            <span className="password_info">
+              *минимум 8 знаков, 1 заглавный, 1 строчный,
+            </span>
+            <span className="password_info second">1 число и 1 символ</span>
+            <div className="error_block">
+              {isError && (
+                <p className="error-message"> Неверная информация!</p>
+              )}
+            </div>
+          </div>
+        </form>
       </div>
-    </form>
-
-  </div>
+    </div>
   );
 }
 
